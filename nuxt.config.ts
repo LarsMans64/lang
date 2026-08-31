8444d6f97b13afb2f63590c6aaeb0c0eaba9f2a9
LarsMans <l.s.vanemmerik@gmail.com>
2026-08-31 21:47:54 +0200

    stuff???????????????????????

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
+  app: {
+    baseURL: '/lang/',
+  },
+
   css: ["assets/main.css"],
   modules: ["@vueuse/nuxt"],
 })
